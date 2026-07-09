5903351193021c2e18ab6ad258306cf01621002d
aditya <adityasingh.sss998@gmail.com>
2026-07-09 13:13:49 +0530

    fixed spell:

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../Context/AuthContext.jsx";
 
 export function useAuth() {
     const context = useContext(AuthContext);
